5001ee6bae5730a9b97100f06a24b30d2c8a8d3d
Jesper Quorning <jesper.quorning@gmail.com>
2023-03-25 09:32:54 +0100

    Update http.ads

--- src/http.ads
+++ src/http.ads
@@ -8,6 +8,22 @@ is
    package Server
    is
       procedure Wait;
+      type HTTP is null record;
+      procedure Start;
+      procedure Stop;
+      procedure Shutdown;
    end Server;
+
+   package Status
+   is
+   end Status;
+
+   package Response
+   is
+   end Status;
+
+   package Services
+   is
+   end Status;
 
 end HTTP;
